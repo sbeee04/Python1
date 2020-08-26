7a86b7e7df9dcd2915cf9953cefcbc967745aaed
sathish babu <sathishbabu31071999@gmail.com>
2020-08-26 19:51:26 +0530

    python
    python basic arithmetic program

---
+++ add.py
@@ -0,0 +1,14 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=a+b
+print("the addition of two numbers is",c)
+d=a-b
+print("the subtraction of two numbers is",d)
+e=a*b
+print("the multiplication of two numbers is",e)
+f=a/b
+print("the division of two numbers is",f)
+
+
+
+
