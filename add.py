e7619f3980002c331753c703cf8d331e103e44f5
ramanuj760 <51733702+ramanuj760@users.noreply.github.com>
2020-10-01 14:41:26 +0530

    Update add.py

--- add.py
+++ add.py
@@ -8,7 +8,16 @@ e=a*b
 print("the multiplication of two numbers is",e)
 f=a/b
 print("the division of two numbers is",f)
-
+g=a**b
+print("b is power of a",a)
+a+=1
+print("it show increment in a",a)
+h=sqrt(a)
+print("square root of a",h)
+i=a//b
+print("it is floor division it shows integer quotent ",i)
+j=a%b
+print("remainder if b divide a ",j)
 
 
 
